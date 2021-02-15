0557143f685a689a30f976e892cec8b535fcd4b5
Jerry Hall <hall.jerry9@gmail.com>
2021-02-15 12:55:25 -0800

    Terraform EC2: Fix to Docker installation

--- terraform/scripts/dev/user_data.sh
+++ terraform/scripts/dev/user_data.sh
@@ -3,11 +3,14 @@
 
 
 # Install Docker
-curl -fsSL https://download.docker.com/linux/ubu...​ | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu​ $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-
+# Allow Docker to run without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
 
 # Install kubectl 
