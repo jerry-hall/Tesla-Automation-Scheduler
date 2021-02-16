4237d04063d106bb535010344c99efc608244fd9
Jerry Hall <hall.jerry9@gmail.com>
2021-02-15 17:57:54 -0800

    Backend: Use body-parser - Required for parsing body of post requests

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
