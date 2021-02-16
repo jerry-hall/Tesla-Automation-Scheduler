4d0e3a0c9a3e246528547d7f6a7c72163062929d
Jerry Hall <hall.jerry9@gmail.com>
2021-02-15 19:45:11 -0800

    Backend: Initial implementation

---
+++ backend/command.js
@@ -0,0 +1,43 @@
+export const handle_command = (request_body) => {
+    var response
+    var now = new Date()
+    var now_str = now.toLocaleString('en-US', { timeZone: 'PST' })
+    var request_datetime = new Date(`${now_str.substring(0,now_str.search(','))}, ${response['execute_at']} PST`)
+    
+    if (request_datetime - now <= 0) {
+        switch (request_body['command']) {
+            case climate:
+                response = await execute_climate_command(request_body)
+                break;
+            case charging:
+                response = await execute_charging_command(request_body)
+                break;
+        
+            default:
+                response = {'code':500, 'message': `Invalid command: ${request_body['command']}`}
+        }
+    } else {
+        response = await schedule_command(request_body)
+    }
+    
+
+    return response
+};
+
+const schedule_command = (request_body) => {
+    // TODO: Implement
+    var response = {'code':200,'message': 'success'}
+    return response
+};
+
+const execute_climate_command = (request_body) => {
+    // TODO: Implement
+    var response = {'code':200,'message': 'success'}
+    return response
+};
+
+const execute_charging_command = (request_body) => {
+    // TODO: Implement
+    var response = {'code':200,'message': 'success'}
+    return response
+};
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const command = require('./command')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,24 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+app.post('/api/1/command', (req, res) => {
+  const required_keys = ["access_token", "command", "execute_at"]
+  var request_body = req.body
+  var missing_elements = []
+  required_keys.forEach(elem => {
+    if (request_body.includes(elem))
+      missing_elements.push(elem)
+  });
+  if (!missing_elements.length){
+    return res.status(400).send({
+      message: `Missing elements: ${missing_elements}` 
+    })}
+  var response = await command.handle_command(request_body)
+  return res.status(response['code']).send({
+    message: response['message'] 
+  })
+})
 
 app.listen(port, () => {
 console.log(`Listening at http://localhost:${port}!`)
